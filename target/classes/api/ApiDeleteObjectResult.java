ab6d49f06d867fd9b749f3c4e5c5499755b62a12
IspecialQ <ivanspecial13@gmail.com>
2022-10-03 23:39:49 +0300

    Ready

---
+++ target/classes/api/ApiDeleteObjectResult.java
@@ -0,0 +1,13 @@
+package api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiDeleteObjectResult {
+    private String status;
+}
